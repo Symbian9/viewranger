3e15b4cec27ddc997673c0d1cef05dc4f7c704de
ardliath <viewrangerapi@gmail.com>
2014-09-08 14:27:26 +0100

    Tests for handling responses to successful calls

--- Liath.ViewRanger.Tests/RequestBuilderTests/GetLastPositionRequestTests/SuccessXmlDownloadedTests.cs
+++ Liath.ViewRanger.Tests/RequestBuilderTests/GetLastPositionRequestTests/SuccessXmlDownloadedTests.cs
@@ -1,5 +1,6 @@
 ﻿using Liath.ViewRanger.Exceptions;
 using Liath.ViewRanger.RequestBuilders;
+using Liath.ViewRanger.Responses;
 using Moq;
 using NUnit.Framework;
 using System;
@@ -25,6 +26,131 @@ namespace Liath.ViewRanger.Tests.RequestBuilderTests.GetLastPositionRequestTests
             request.CallBase = true;
             request.Setup(x => x.MakeRequest(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(this.GetXDocument(filename));
             Assert.Throws<UnexpectedResponseException>(() => request.Object.Request());
+        }
+
+        #region Properties
+
+        [Test]
+        public void Check_Latitude()
+        {
+            var location = this.GetLocationFromSuccessfulResponse();
+            Assert.AreEqual(52.2087M, location.Latitude);
+        }
+
+        [Test]
+        public void Check_Speed()
+        {
+            var location = this.GetLocationFromSuccessfulResponse();
+            Assert.AreEqual(1M, location.Speed);
+        }
+
+        [Test]
+        public void Check_Heading()
+        {
+            var location = this.GetLocationFromSuccessfulResponse();
+            Assert.AreEqual(270M, location.Heading);
+        }
+
+        [Test]
+        public void Check_Altitude()
+        {
+            var location = this.GetLocationFromSuccessfulResponse();
+            Assert.AreEqual(10M, location.Altitude);
+        }
+
+        [Test]
+        public void Check_Date()
+        {
+            var location = this.GetLocationFromSuccessfulResponse();
+            Assert.AreEqual(new DateTime(2011, 09, 22, 09, 52, 58), location.Date);
+        }
+
+        #endregion
+
+        #region Nulls
+
+        [Test]
+        public void Check_Latitude_is_null_when_ommitted()
+        {
+            var location = this.GetLocationFromEmptyResponse();
+            Assert.IsNull(location.Latitude);
+        }
+
+        [Test]
+        public void Check_Longitude_is_null_when_ommitted()
+        {
+            var location = this.GetLocationFromEmptyResponse();
+            Assert.IsNull(location.Longitude);
+        }
+
+        [Test]
+        public void Check_Speed_is_null_when_ommitted()
+        {
+            var location = this.GetLocationFromEmptyResponse();
+            Assert.IsNull(location.Speed);
+        }
+
+        [Test]
+        public void Check_Altitude_is_null_when_ommitted()
+        {
+            var location = this.GetLocationFromEmptyResponse();
+            Assert.IsNull(location.Altitude);
+        }
+
+        [Test]
+        public void Check_Heading_is_null_when_ommitted()
+        {
+            var location = this.GetLocationFromEmptyResponse();
+            Assert.IsNull(location.Heading);
+        }
+
+        [Test]
+        public void Check_Date_is_null_when_ommitted()
+        {
+            var location = this.GetLocationFromEmptyResponse();
+            Assert.IsNull(location.Date);
+        }
+
+        #endregion
+        
+        [TestCase("LATITUDE")]
+        [TestCase("LONGITUDE")]
+        [TestCase("DATE")]
+        [TestCase("ALTITUDE")]
+        [TestCase("SPEED")]
+        [TestCase("HEADING")]
+        public void Ensure_throws_when_value_cannot_be_parsed(string node)
+        {            
+            Assert.Throws<UnexpectedResponseException>(() =>
+                {
+                    var location = this.GetLocationFromInvalidResponse(node);
+                });
+        }
+
+        private Location GetLocationFromSuccessfulResponse()
+        {
+            var request = new Mock<GetLastPositionRequest>(Guid.NewGuid().ToString());
+            request.CallBase = true;
+            request.Setup(x => x.MakeRequest(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(this.GetXDocument("Successful.xml"));
+            return request.Object.Request();
+        }
+
+        private Location GetLocationFromInvalidResponse(string nodeName)
+        {
+            var xml = this.GetXDocument("Successful.xml");
+            xml.Descendants(nodeName).Single().Value = Guid.NewGuid().ToString();
+            var request = new Mock<GetLastPositionRequest>(Guid.NewGuid().ToString());
+            request.CallBase = true;
+            request.Setup(x => x.MakeRequest(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(xml);
+            return request.Object.Request();
+        }
+
+        private Location GetLocationFromEmptyResponse()
+        {
+            var request = new Mock<GetLastPositionRequest>(Guid.NewGuid().ToString());
+            request.CallBase = true;
+            request.Setup(x => x.MakeRequest(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(this.GetXDocument("Empty.xml"));
+            return request.Object.Request();
         }
 
         private XDocument GetXDocument(string name)
