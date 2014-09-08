bc3ab012f365b2b7d7949e72995ae36bfbe051dd
ardliath <viewrangerapi@gmail.com>
2014-09-08 14:46:16 +0100

    Added test to make sure the xml is being downloaded

---
+++ Liath.ViewRanger.Tests/RequestBuilderTests/RequestBaseTests/MakeRequestTests.cs
@@ -0,0 +1,36 @@
+﻿using Liath.ViewRanger.RequestBuilders;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Liath.ViewRanger.Tests.RequestBuilderTests.RequestBaseTests
+{
+    [TestFixture]
+    public class MakeRequestTests
+    {
+        [Test]
+        public void Ensure_MakeRequest_downloads_xml()
+        {
+            var urlToCall = Guid.NewGuid().ToString();
+            var xml = GetXDocument();
+            var request = new Mock<GetLastPositionRequest>(Guid.NewGuid().ToString());
+            request.CallBase = true;
+            request.Setup(x => x.CreateUrl(It.IsAny<RequestParameter[]>())).Returns(urlToCall);
+            request.Setup(x => x.DownloadXml(urlToCall)).Returns(xml).Verifiable();
+
+            request.Object.ForUser(Guid.NewGuid().ToString(), Guid.NewGuid().ToString()).Request();
+
+            request.Verify(x => x.DownloadXml(urlToCall), Times.Once());
+        }
+
+        private XDocument GetXDocument()
+        {
+            return XDocument.Load(@"RequestBuilderTests\GetLastPositionRequestTests\RequestTests\SampleResponses\Successful.xml");
+        }
+    }
+}
