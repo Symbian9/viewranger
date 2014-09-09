e26011f09161b58f895f8db0622f7f7fd977eee7
ardliath <viewrangerapi@gmail.com>
2014-09-09 14:03:28 +0100

    Added some extra logging

--- Build.bat
+++ Build.bat
@@ -14,12 +14,21 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+echo Restoring NuGet packages
 %nuget% restore
  
+echo Building Solution
+ 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Liath.ViewRanger.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+ 
+echo Build Complete
  
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
- 
+
+echo Packaging 
+
 %nuget% pack "ViewRanger.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+
+echo Done!
