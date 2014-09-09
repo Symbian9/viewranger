fb1290cd337f96527783e0e8172a5ad2f980e0aa
ardliath <viewrangerapi@gmail.com>
2014-09-09 14:52:18 +0100

    Began to implement the GetTrack mechanism
    Tracks are strictly a collection of BuddyBeacon points, we may need to
    rename this later

---
+++ Liath.ViewRanger/RequestBuilders/GetTrackRequest.cs
@@ -0,0 +1,34 @@
+﻿using Liath.ViewRanger.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liath.ViewRanger.RequestBuilders
+{
+    public class GetTrackRequest : RequestBase, IGetTrackRequest
+    {
+        public GetTrackRequest(string apiKey)
+            : base(apiKey)
+        {
+
+        }
+
+        public override string Service
+        {
+            get { return "getBBPositions"; }
+        }
+
+        public Track Request()
+        {
+            throw new NotImplementedException();
+        }        
+
+
+        //http://api.viewranger.com/public/v1/?key={API-KEY}&service=getBBPositions&username={BB-USERNAME}&pin={BB-PIN}&date_from={DATE-FROM}&date_until={DATE-UNTIL}&limit={LIMIT}&format={FORMAT}#sthash.kKhQV9EG.dpuf
+
+        //<?xml version="1.0" encoding="UTF-8"?> <VIEWRANGER> <LOCATION> <LATITUDE>51.4609</LATITUDE> <LONGITUDE>-2.58858</LONGITUDE> <DATE>2011-09-21 15:11:05</DATE> <ALTITUDE>0</ALTITUDE> <SPEED>0</SPEED> <HEADING>188</HEADING> </LOCATION> <LOCATION> <LATITUDE>51.4609</LATITUDE> <LONGITUDE>-2.58854</LONGITUDE> <DATE>2011-09-21 15:07:05</DATE> <ALTITUDE>0</ALTITUDE> <SPEED>0</SPEED> <HEADING>8</HEADING> </LOCATION> <LOCATION> <LATITUDE>51.4609</LATITUDE> <LONGITUDE>-2.58858</LONGITUDE> <DATE>2011-09-21 15:03:05</DATE> <ALTITUDE>0</ALTITUDE> <SPEED>0</SPEED> <HEADING>205</HEADING> </LOCATION> </VIEWRANGER> - See more at: http://www.viewranger.com/developers/documentation/#sthash.kKhQV9EG.dpuf
+
+    }
+}
---
+++ Liath.ViewRanger/RequestBuilders/IGetTrackRequest.cs
@@ -0,0 +1,12 @@
+﻿using Liath.ViewRanger.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Liath.ViewRanger.RequestBuilders
+{
+    public interface IGetTrackRequest : IRequest<Track>
+    {
+    }
+}
