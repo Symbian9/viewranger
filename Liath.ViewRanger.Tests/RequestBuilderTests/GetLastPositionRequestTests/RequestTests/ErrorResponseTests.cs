d4e17a5a6066ef2c7b4233f01277f70eba73af4e
ardliath <viewrangerapi@gmail.com>
2014-09-11 09:23:25 +0100

    Tested the GetLastPosition error responses

--- Liath.ViewRanger.Tests/RequestBuilderTests/GetLastPositionRequestTests/RequestTests/ErrorResponseTests.cs
+++ Liath.ViewRanger.Tests/RequestBuilderTests/GetLastPositionRequestTests/RequestTests/ErrorResponseTests.cs
@@ -1,7 +1,7 @@
 ﻿using Liath.ViewRanger.Exceptions;
 using Liath.ViewRanger.RequestBuilders;
 using Liath.ViewRanger.Responses;
-using Liath.ViewRanger.Tests.RequestBuilderTests.GetTrackRequestTests.RequestTests.SampleResponses;
+using Liath.ViewRanger.Tests.RequestBuilderTests.GetLastPositionRequestTests.RequestTests.SampleResponses;
 using Liath.ViewRanger.Tests.RequestBuilderTests.RequestBaseTests;
 using Moq;
 using NUnit.Framework;
@@ -11,20 +11,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Liath.ViewRanger.Tests.RequestBuilderTests.GetLastPositionRequestTests.RequestTests
+namespace Liath.ViewRanger.Tests.RequestBuilderTests.GetTrackRequestTests.RequestTests
 {
     [TestFixture]
-    public class ErrorResponseTests : ErrorTesting<IGetTrackRequest, Track>
+    public class ErrorResponseTests : ErrorTesting<IGetLastPositionRequest, Location>
     {
         // This class ensures that all GetTrackRequest error messages are checked, the BaseClass actually does the implementation
         // of the tests - after all the same codes should produce the same errors for both!
 
-        public override IGetTrackRequest CreateRequest(string code)
+        public override IGetLastPositionRequest CreateRequest(string code)
         {
             var xml = SampleResponse.Error;
             xml.Descendants("CODE").Single().Value = code;
             xml.Descendants("MESSAGE").Single().Value = Guid.NewGuid().ToString();
-            var request = new Mock<GetTrackRequest>(Guid.NewGuid().ToString());
+            var request = new Mock<GetLastPositionRequest>(Guid.NewGuid().ToString());
             request.CallBase = true;
             request.Setup(x => x.DownloadXml(It.IsAny<string>())).Returns(xml);
             return request.Object.ForUser(Guid.NewGuid().ToString(), Guid.NewGuid().ToString());
---
+++ Liath.ViewRanger.Tests/RequestBuilderTests/RequestBaseTests/ErrorTesting.cs
@@ -0,0 +1,57 @@
+﻿using Liath.ViewRanger.Exceptions;
+using Liath.ViewRanger.RequestBuilders;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liath.ViewRanger.Tests.RequestBuilderTests.RequestBaseTests
+{
+    public abstract class ErrorTesting<TRequestType, TReturnType> where TRequestType : IRequest<TReturnType>
+    {
+        public abstract TRequestType CreateRequest(string code);
+
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("5")]
+        public void Ensure_code_throws_ApiKeyRejectedException(string code)
+        {
+            var request = this.CreateRequest(code);
+            Assert.Throws<ApiKeyRejectedException>(() => request.Request());
+        }
+
+        [TestCase("10")]
+        [TestCase("11")]
+        public void Ensure_code_throws_InvalidUserCredentials(string code)
+        {
+            var request = this.CreateRequest(code);
+            Assert.Throws<InvalidUserCredentialsException>(() => request.Request());
+        }
+
+        [TestCase("4")]
+        [TestCase("8")]
+        [TestCase("9")]
+        public void Ensure_code_throws_MalformedRequestException(string code)
+        {
+            var request = this.CreateRequest(code);
+            Assert.Throws<MalformedRequestException>(() => request.Request());
+        }
+
+        [TestCase("6")]
+        public void Ensure_code_throws_InternalViewRangerException(string code)
+        {
+            var request = this.CreateRequest(code);
+            Assert.Throws<InternalViewRangerException>(() => request.Request());
+        }
+
+        [TestCase("15")] // A code we don't recognise
+        public void Ensure_code_throws_FailedRequestException(string code)
+        {
+            var request = this.CreateRequest(code);
+            Assert.Throws<FailedRequestException>(() => request.Request());
+        }
+    }
+}
