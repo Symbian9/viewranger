65e45368a9e8d9992d7ed3c61bd0d57bee6ff574
ardliath <viewrangerapi@gmail.com>
2014-09-11 09:47:43 +0100

    Updated Exception Inheritance

--- Liath.ViewRanger/Exceptions/ClientException.cs
+++ Liath.ViewRanger/Exceptions/ClientException.cs
@@ -9,7 +9,7 @@ namespace Liath.ViewRanger.Exceptions
     /// <summary>
     /// The exception thrown if there was a problem with this library
     /// </summary>
-    public class ClientException : Exception
+    public class ClientException : ViewRangerException
     {
         public ClientException(Exception innerException)
             : base("There was an error in the ViewRanger API Client Library, see the InnerException for more details", innerException)
--- Liath.ViewRanger/Exceptions/ViewRangerException.cs
+++ Liath.ViewRanger/Exceptions/ViewRangerException.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 namespace Liath.ViewRanger.Exceptions
 {
     /// <summary>
-    /// All Exceptions from ViewRanger inherit from this
+    /// All Exceptions thrown by this library should inherit from this
     /// </summary>
     public class ViewRangerException : Exception
     {
         public ViewRangerException(string message)
             :base(message)
+        {
+
+        }
+
+        public ViewRangerException(string message, Exception innerException)
+            : base(message, innerException)
         {
 
         }
