9177a5fa69b430bb84cbb29fa8a5cdb539a086cd
ardliath <viewrangerapi@gmail.com>
2014-09-16 11:49:32 +0100

    Implemented HasCredentials

---
+++ Liath.ViewRanger.Tests/IUserExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liath.ViewRanger.Tests
+{
+    [TestFixture]
+    public class IUserExtensionsTests
+    {
+        [Test]
+        public void Ensure_false_when_username_null()
+        {
+            Assert.IsFalse(new UserAccount(null, Guid.NewGuid().ToString()).HasCredentials());
+        }
+
+        [Test]
+        public void Ensure_false_when_pin_null()
+        {
+            Assert.IsFalse(new UserAccount(Guid.NewGuid().ToString(), null).HasCredentials());
+        }
+
+        [Test]
+        public void Ensure_false_when_username_white()
+        {
+            Assert.IsFalse(new UserAccount("             ", Guid.NewGuid().ToString()).HasCredentials());
+        }
+
+        [Test]
+        public void Ensure_false_when_pin_white()
+        {
+            Assert.IsFalse(new UserAccount(Guid.NewGuid().ToString(), "    ").HasCredentials());
+        }
+
+        [Test]
+        public void Ensure_false_when_both_null()
+        {
+            Assert.IsFalse(new UserAccount(null, null).HasCredentials());
+        }
+
+        [Test]
+        public void Ensure_false_when_both_populated()
+        {
+            Assert.IsTrue(new UserAccount(Guid.NewGuid().ToString(), Guid.NewGuid().ToString()).HasCredentials());
+        }
+
+
+        /// <summary>
+        /// Quick class to test the IUser, saves mocking it over and over again
+        /// </summary>
+        private class UserAccount : IUser
+        {
+            private string _username;
+            private string _pin;
+
+            public UserAccount(string username, string pin)
+            {
+                _username = username;
+                _pin = pin;
+            }
+
+            public string BuddyBeaconUsername
+            {
+                get { return _username; }
+            }
+
+            public string BuddyBeaconPin
+            {
+                get { return _pin; }
+            }
+        }
+    }
+}
---
+++ Liath.ViewRanger/IUserExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liath.ViewRanger
+{
+    /// <summary>
+    /// Contains Extension methods for IUser
+    /// </summary>
+    public static class IUserExtensions
+    {
+        /// <summary>
+        /// Returns a bool indicating whether the specified user has their BuddyBeacon username and password populated
+        /// </summary>
+        public static bool HasCredentials(this IUser user)
+        {
+            return !string.IsNullOrWhiteSpace(user.BuddyBeaconUsername)
+                && !string.IsNullOrWhiteSpace(user.BuddyBeaconPin);
+        }
+    }
+}
